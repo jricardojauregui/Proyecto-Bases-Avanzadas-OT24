263be2e648ae19508b57f21f3b5eec434e765a84
José Ricardo Jáuregui Guevara <jose.jauregui@udem.edu>
2024-11-11 18:32:32 -0600

    Añadí mas url routes

--- app.py
+++ app.py
@@ -12,3 +12,27 @@ app.secret_key = SECRET_KEY
 
 init_app(app)
 
+### RUTA LANDING
+@app.route('/')
+def inicio():
+    return redirect(url_for('inicio'))
+
+### RUTAS ADMIN
+app.add_url_rule('/loginAdmin', view_func=loginAdmin, methods=['GET', 'POST'])
+app.add_url_rule('/registerAdmin', view_func=registerAdmin, methods=['GET', 'POST'])
+app.add_url_rule('/logoutAdmin', view_func=logoutAdmin)
+app.add_url_rule('/empresas', view_func=mostrarEmpresas)
+app.add_url_rule('/editarEmpresas/<int:id>', view_func=editarEmpresas, methods=['GET', 'POST'])
+app.add_url_rule('/eliminarEmpresas/<int:id>', view_func=eliminarEmpresas, methods=['GET', 'POST'])
+
+### RUTAS EMPRESAS
+app.add_url_rule('/loginEmp', view_func=loginEmp, methods=['GET', 'POST'])
+app.add_url_rule('/registrarEmp', view_func=registerEmp, methods=['GET', 'POST'])
+app.add_url_rule('/logoutEmp', view_func=logoutEmp)
+app.add_url_rule('/editarEmp/<int:id>', view_func=editarEmp, methods=['GET', 'POST'])
+
+### RUTAS USUARIO
+app.add_url_rule('/login', view_func=loginU, methods=['GET', 'POST'])
+app.add_url_rule('/registrar', view_func=registerU, methods=['GET', 'POST'])
+app.add_url_rule('/logoutEmp', view_func=logoutU)
+app.add_url_rule('/compra', view_func=compra, methods=['GET', 'POST'])
