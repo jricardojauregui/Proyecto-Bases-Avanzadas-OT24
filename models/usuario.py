89e735e345c11d8498aea5a6a74acd6ee7df4f10
José Ricardo Jáuregui Guevara <jose.jauregui@udem.edu>
2024-11-19 19:24:01 -0600

    .

--- models/usuario.py
+++ models/usuario.py
@@ -5,27 +5,6 @@ import hashlib
 
 class Usuario:
     @staticmethod
-    def insertUsuario(clave, nom_usr, apellido_usr, correo_usr, tel_usr, tel_domicilio, direccion, foto_usuario): 
-        cur = mysql.connection.cursor()
-        cur.callproc('InsertUsuario', (clave, nom_usr, apellido_usr, correo_usr, tel_usr, tel_domicilio, direccion, foto_usuario)) 
-        mysql.connection.commit()
-        cur.close
-
-    @staticmethod
-    def updateUsuario(id_usr, clave, nom_usr, apellido_usr, correo_usr, tel_usr, tel_domicilio, direccion, foto_usuario): 
-        cur = mysql.connection.cursor()
-        cur.callproc('UpdateUsuario', (id_usr, clave, nom_usr, apellido_usr, correo_usr, tel_usr, tel_domicilio, direccion, foto_usuario)) 
-        mysql.connection.commit()
-        cur.close
-
-    @staticmethod
-    def deleteUsuario(id_usr): 
-        cur = mysql.connection.cursor()
-        cur.callproc('DeleteUsuario', (id_usr,)) 
-        mysql.connection.commit()
-        cur.close
-
-    @staticmethod
     def get_all():
         cur = mysql.connection.cursor()
         cur.callproc('MostrarUsuarios') 
