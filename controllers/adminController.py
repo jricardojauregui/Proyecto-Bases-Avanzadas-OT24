a0211c137f79dfd5f3b0b4c25882412e2ceef837
José Ricardo Jáuregui Guevara <jose.jauregui@udem.edu>
2024-11-27 00:33:42 -0600

    Avance

--- controllers/adminController.py
+++ controllers/adminController.py
@@ -4,15 +4,6 @@ from models.usuario import login_user, get_all_products
 from functools import wraps
 import hashlib
 
-def admin_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if not session.get('loggedin') or not session.get('is_admin'):
-            flash('Debes iniciar sesión como administrador para acceder a esta página.', 'error')
-            return redirect(url_for('admin_login'))  
-        return f(*args, **kwargs)
-    return decorated_function
-
 def admin_login():
     if request.method == 'POST':
         username = request.form.get('username')
@@ -37,113 +28,112 @@ def admin_login():
 
     return render_template('admin_login.html')
 
+
 def admin_logout():
     session.clear()
     flash('Sesión cerrada correctamente.', 'success')
     return redirect(url_for('admin_login'))
 
-@admin_required
+
 def admin_view_dash():
-    return render_template('dashboard.html')
+    if 'loggedin' in session and session.get('is_admin'):
+        return render_template('dashboard.html')
+    else:
+        flash('Debes iniciar sesión como administrador para acceder a esta página.', 'error')
+        return redirect(url_for('admin_login'))
 
-@admin_required
+
 def promedio_calificacion_productos():
-    resultado = get_promedio_calificacion_productos()
-    return jsonify([{"name": row[0], "y": float(row[1])} for row in resultado if row[1] is not None])
+    if 'loggedin' in session and session.get('is_admin'):
+        resultado = get_promedio_calificacion_productos()
+        return jsonify([{"name": row[0], "y": float(row[1])} for row in resultado if row[1] is not None])
+    else:
+        flash('Debes iniciar sesión como administrador para acceder a esta página.', 'error')
+        return redirect(url_for('admin_login'))
 
-@admin_required
+
 def cantidad_categorias_productos():
-    resultado = get_cantidad_categorias_productos()
-    return jsonify([{"name": row[0], "y": float(row[1])} for row in resultado if row[1] is not None])
+    if 'loggedin' in session and session.get('is_admin'):
+        resultado = get_cantidad_categorias_productos()
+        return jsonify([{"name": row[0], "y": float(row[1])} for row in resultado if row[1] is not None])
+    else:
+        flash('Debes iniciar sesión como administrador para acceder a esta página.', 'error')
+        return redirect(url_for('admin_login'))
 
-@admin_required
-def cantidad_stock_productos():
-    resultado = get_cantidad_stock_productos()
-    return jsonify([{"name": row[0], "y": float(row[1])} for row in resultado if row[1] is not None])
 
-@admin_required
-def pedidos_por_mes():
-    resultado = get_pedidos_por_mes()
-    return jsonify([{"name": row[0], "y": float(row[1])} for row in resultado if row[1] is not None])
-
-@admin_required
-def usuarios_por_mes():
-    resultado = get_usuarios_por_mes()
-    return jsonify([{"name": row[0], "y": float(row[1])} for row in resultado if row[1] is not None])
-
-@admin_required
-def productos_estado(estado):
-    resultado = get_productos_estado(estado)
-    return jsonify([{"total": resultado[0]}])
-
-@admin_required
-def ganancias_por_mes():
-    resultado = get_ganancias_por_mes()
-    return jsonify([{"name": row[0], "y": float(row[1])} for row in resultado if row[1] is not None])
-
-@admin_required
 def admin_manage_users():
-    if request.method == 'POST':
-        if 'field' in request.form:
-            id_usr = request.form.get('id_usr')
-            field = request.form.get('field')
-            value = request.form.get('value')
+    if 'loggedin' in session and session.get('is_admin'):
+        if request.method == 'POST':
+            if 'field' in request.form:
+                id_usr = request.form.get('id_usr')
+                field = request.form.get('field')
+                value = request.form.get('value')
 
-            if not id_usr or not field or value is None:
-                return jsonify({"success": False, "message": "Datos incompletos."})
+                if not id_usr or not field or value is None:
+                    return jsonify({"success": False, "message": "Datos incompletos."})
 
-            success, message = update_user_field(id_usr, field, value)
-            return jsonify({"success": success, "message": message})
+                success, message = update_user_field(id_usr, field, value)
+                return jsonify({"success": success, "message": message})
 
-        if 'delete_user' in request.form:
-            id_usr = request.form.get('delete_user')
+            if 'delete_user' in request.form:
+                id_usr = request.form.get('delete_user')
 
-            if not id_usr:
-                return jsonify({"success": False, "message": "No se especificó el usuario a eliminar."})
+                if not id_usr:
+                    return jsonify({"success": False, "message": "No se especificó el usuario a eliminar."})
 
-            success, message = delete_user_by_id(id_usr)
-            return jsonify({"success": success, "message": message})
+                success, message = delete_user_by_id(id_usr)
+                return jsonify({"success": success, "message": message})
 
-    users = get_all_users()
-    return render_template('manage_users.html', users=users)
+        users = get_all_users()
+        return render_template('manage_users.html', users=users)
+    else:
+        flash('Debes iniciar sesión como administrador para acceder a esta página.', 'error')
+        return redirect(url_for('admin_login'))
 
-@admin_required
+
 def admin_manage_products():
+    if 'loggedin' in session and session.get('is_admin'):
+        if request.method == 'POST':
+            if 'delete_product' in request.form:
+                id_producto = request.form.get('delete_product')
 
-    if request.method == 'POST':
-        if 'delete_product' in request.form:
-            id_producto = request.form.get('delete_product')
+                if not id_producto:
+                    return jsonify({"success": False, "message": "No se especificó el producto a eliminar."})
 
-            if not id_producto:
-                return jsonify({"success": False, "message": "No se especificó el producto a eliminar."})
+                success, message = delete_product_by_id(id_producto)
+                return jsonify({"success": success, "message": message})
 
-            success, message = delete_product_by_id(id_producto)
-            return jsonify({"success": success, "message": message})
+        products = get_all_products()
+        return render_template('manage_products.html', products=products)
+    else:
+        flash('Debes iniciar sesión como administrador para acceder a esta página.', 'error')
+        return redirect(url_for('admin_login'))
 
-    products = get_all_products()
-    return render_template('manage_products.html', products=products)
 
-@admin_required
 def admin_manage_orders():
-    if request.method == 'POST':
-        if 'estado_pedido' in request.form:
-            id_pedido = request.form.get('id_pedido')
-            estado_pedido = request.form.get('estado_pedido')
+    if 'loggedin' in session and session.get('is_admin'):
+        if request.method == 'POST':
+            if 'estado_pedido' in request.form:
+                id_pedido = request.form.get('id_pedido')
+                estado_pedido = request.form.get('estado_pedido')
 
-            if not id_pedido or not estado_pedido:
-                return jsonify({"success": False, "message": "Datos incompletos."})
+                if not id_pedido or not estado_pedido:
+                    return jsonify({"success": False, "message": "Datos incompletos."})
 
-            success, message = update_order_status(id_pedido, estado_pedido)
-            return jsonify({"success": success, "message": message})
+                success, message = update_order_status(id_pedido, estado_pedido)
+                return jsonify({"success": success, "message": message})
 
-        if 'delete_order' in request.form:
-            id_pedido = request.form.get('delete_order')
+            if 'delete_order' in request.form:
+                id_pedido = request.form.get('delete_order')
 
-            if not id_pedido:
-                return jsonify({"success": False, "message": "No se especificó la orden a eliminar."})
+                if not id_pedido:
+                    return jsonify({"success": False, "message": "No se especificó la orden a eliminar."})
 
-            success, message = delete_order_by_id(id_pedido)
-            return jsonify({"success": success, "message": message})
+                success, message = delete_order_by_id(id_pedido)
+                return jsonify({"success": success, "message": message})
 
-    orders = get_all_orders()
-    return render_template('manage_orders.html', orders=orders)
+        orders = get_all_orders()
+        return render_template('manage_orders.html', orders=orders)
+    else:
+        flash('Debes iniciar sesión como administrador para acceder a esta página.', 'error')
+        return redirect(url_for('admin_login'))
